b4514acf214b4d1f7d1d92d33d8dc4f15ae00156
Brett Bukowski <brett.bukowski@gmail.com>
2014-12-17 21:14:52 -0700

    Add activity route / view

--- app/routes/activity.js
+++ app/routes/activity.js
@@ -1,4 +1,7 @@
 import ProtectedRoute from './protected';
 
 export default ProtectedRoute.extend({
+  model: function () {
+    return Ember.$.getJSON(UriTemplate.expand(this.get('session').get('currentUser').received_events_url));
+  }
 });
