3fea1fc8ce1583e655e8611dc316c735d8c581ac
Brett Bukowski <brett.bukowski@gmail.com>
2015-01-23 12:14:56 -0800

    Add events boilerplate serializer / adapter / model

--- app/routes/you.js
+++ app/routes/you.js
@@ -1,8 +1,7 @@
-import Ember from 'ember';
 import ProtectedRoute from './protected';
 
 export default ProtectedRoute.extend({
   model: function () {
-    return Ember.$.getJSON(UriTemplate.expand(this.get('session').get('currentUser').events_url));
+    return this.store.findAll('event');
   }
 });
